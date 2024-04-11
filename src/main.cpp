0e44eef7beda159f3be8de967d8ab581024349a0
BidoiaSimone <117077053+BidoiaSimone@users.noreply.github.com>
2024-04-11 12:01:45 +0200

    implemented project hierarchy and gitignore

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+#include "json.cpp"
+
+int main(){
+    try{
+
+        json j;
+        json j2;
+        json j3;
+        std::ifstream f("input.txt", std::ios::in);
+        std::ofstream o("output.txt", std::ios::out);
+        f >> j;
+        f >> j2;
+        std::cout << j << std::endl << "-----------------------------------------" << std::endl;
+        std::cout << j2 << std::endl << "-----------------------------------------" << std::endl;
+        std::cout << j3 << std::endl;
+
+
+
+    }
+    catch(json_exception error){
+        std::cout << std::endl << "-----------------------------------------"
+                  << std::endl << error.msg << std::endl << "-----------------------------------------" << std::endl;
+    }
+
+    return 0;
+}
